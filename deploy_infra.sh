721c88393f067dc6d91457b338ed23a324a70185
Berke Tezergil <btezergil@atlassian.com>
2022-10-10 16:18:12 +0300

    New updates for CodeDeploy

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -10,6 +10,13 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
+# Generate a personal access token with repo and admin:repo_hook
+#    permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-exercises-access-token)
+GH_OWNER=$(cat ~/.github/aws-exercises-owner)
+GH_REPO=$(cat ~/.github/aws-exercises-repo)
+GH_BRANCH=master
+
 # Deploys static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
@@ -31,7 +38,13 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
     # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
